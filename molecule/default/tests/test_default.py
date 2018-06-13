48fce4438d770670ba895c07f43448f6cf002d1a
coaxial <hi@64b.it>
2018-06-12 21:19:39 -0400

    Test service

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -12,3 +12,10 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+def test_node_exporter_running(host):
+    s = host.service("node_exporter")
+
+    assert s.is_running
+    assert s.is_enabled
